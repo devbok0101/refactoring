480bf866049ceeceb36dcf8994b6e0e0d5f42363
devbok0101 <devbok0209@bodycodi.com>
2023-06-22 20:17:58 +0900

    [codeSmell03] recode 내에 메서드 추가 가능

--- src/main/java/me/whiteship/refactoring/_01_smell_mysterious_name/_03_rename_field/StudyReview.java
+++ src/main/java/me/whiteship/refactoring/_01_smell_mysterious_name/_03_rename_field/StudyReview.java
@@ -1,6 +1,7 @@
 package me.whiteship.refactoring._01_smell_mysterious_name._03_rename_field;
 
 public record StudyReview(String reviewer, String review) {
-
-    
+    public String hello() {
+        return "hello";
+    }
 }

0b8cfc480061314ef97b6b894664b038c99559a6
devbok0101 <devbok0101@gmail.com>
2023-06-23 08:46:15 +0900

    [codeSmell03] recode 내 사용하지 않는 메서드 제거

--- src/main/java/me/whiteship/refactoring/_01_smell_mysterious_name/_03_rename_field/StudyReview.java
+++ src/main/java/me/whiteship/refactoring/_01_smell_mysterious_name/_03_rename_field/StudyReview.java
@@ -1,7 +1,4 @@
 package me.whiteship.refactoring._01_smell_mysterious_name._03_rename_field;
 
 public record StudyReview(String reviewer, String review) {
-    public String hello() {
-        return "hello";
-    }
 }

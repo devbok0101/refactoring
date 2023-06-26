828be46a5ab52cfb9d906de5a8afee85df02d395
devbok0101 <devbok0101@gmail.com>
2023-06-26 20:49:19 +0900

    [codeSmell06] 하위 클래스에서 중복 코드를 발견해서, 상위 클래스로 올리는 리팩토링 = pull members up

--- src/main/java/me/whiteship/refactoring/_02_duplicated_code/_06_pull_up_method/ReviewerDashboard.java
+++ src/main/java/me/whiteship/refactoring/_02_duplicated_code/_06_pull_up_method/ReviewerDashboard.java
@@ -9,23 +9,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ReviewerDashboard extends Dashboard {
-
     public void printReviewers() throws IOException {
-        printUserName(30);
+        super.printUserNames(30);
     }
-
-    private void printUserName(int eventId) throws IOException {
-        // Get github issue to check homework
-        GitHub gitHub = GitHub.connect();
-        GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        GHIssue issue = repository.getIssue(eventId);
-
-        // Get userNames
-        Set<String> userNames = new HashSet<>();
-        issue.getComments().forEach(c -> userNames.add(c.getUserName()));
-
-        // Print userNames
-        userNames.forEach(System.out::println);
-    }
-
 }

40c348cc3c5917d8464050d4e444873def6adae4
devbok0101 <devbok0101@gmail.com>
2023-06-29 23:49:19 +0900

    [codeSmell12] 반복되는 로컬 변수는 필드로 변경해 매개변수를 줄일 수 있음

--- src/main/java/me/whiteship/refactoring/_03_long_function/_12_split_loop/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_03_long_function/_12_split_loop/StudyDashboard.java
@@ -32,8 +32,12 @@ public class StudyDashboard {
     }
 
     private void print() throws IOException, InterruptedException {
-        GHRepository ghRepository = getGhRepository();
+        checkGithubIssues( getGhRepository());
+        new StudyPrinter(this.totalNumberOfEvents, this.participants).execute();
+        printFirstParticipants();
+    }
 
+    private void checkGithubIssues(GHRepository ghRepository) throws InterruptedException {
         ExecutorService service = Executors.newFixedThreadPool(8);
         CountDownLatch latch = new CountDownLatch(totalNumberOfEvents);
 
@@ -57,9 +61,6 @@ public class StudyDashboard {
 
         latch.await();
         service.shutdown();
-
-        new StudyPrinter(this.totalNumberOfEvents, this.participants).execute();
-        printFirstParticipants();
     }
 
     private Participant findFirst(List<GHIssueComment> comments) throws IOException {

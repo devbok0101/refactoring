33288dd5818b9f8732c0492298acc4a5a130f7d5
devbok0101 <devbok0101@gmail.com>
2023-06-30 00:13:59 +0900

    [codeSmell13] StudyPrinter 추상 클래스 변경

--- src/main/java/me/whiteship/refactoring/_03_long_function/_13_replace_conditional_with_polymorphism/StudyPrinter.java
+++ src/main/java/me/whiteship/refactoring/_03_long_function/_13_replace_conditional_with_polymorphism/StudyPrinter.java
@@ -1,12 +1,10 @@
 package me.whiteship.refactoring._03_long_function._13_replace_conditional_with_polymorphism;
 
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Comparator;
 import java.util.List;
 
-public class StudyPrinter {
+public abstract class StudyPrinter {
 
     protected int totalNumberOfEvents;
 
@@ -18,8 +16,20 @@ public class StudyPrinter {
         this.participants.sort(Comparator.comparing(Participant::username));
     }
 
-    public void execute() throws IOException {
+    public abstract void execute() throws IOException;
 
+    /**
+     * |:white_check_mark:|:white_check_mark:|:white_check_mark:|:x:|
+     */
+    protected String checkMark(Participant p) {
+        StringBuilder line = new StringBuilder();
+        for (int i = 1 ; i <= this.totalNumberOfEvents ; i++) {
+            if(p.homework().containsKey(i) && p.homework().get(i)) {
+                line.append("|:white_check_mark:");
+            } else {
+                line.append("|:x:");
+            }
+        }
+        return line.toString();
     }
-
 }

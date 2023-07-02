c2e29d144007f51f4e7eb4203ea04f10abdff92f
devbok0101 <devbok0101@gmail.com>
2023-07-02 21:09:15 +0900

    [codeSmell22] 강의와 동일하게 클래스 구조 변경

--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_22_combine_functions_into_transform/Client3.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_22_combine_functions_into_transform/Client3.java
@@ -3,7 +3,7 @@ package me.whiteship.refactoring._06_mutable_data._22_combine_functions_into_tra
 import java.time.Month;
 import java.time.Year;
 
-public class Client3 {
+public class Client3 extends ReadingClient {
 
     private double basicChargeAmount;
 
@@ -13,10 +13,6 @@ public class Client3 {
 
     private double calculateBaseCharge(Reading reading) {
         return baseRate(reading.month(), reading.year()) * reading.quantity();
-    }
-
-    private double baseRate(Month month, Year year) {
-        return 10;
     }
 
     public double getBasicChargeAmount() {

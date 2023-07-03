ca2f46857fd03f5f2a0aab269ee1fa85c1cabf2d
devbok0101 <devbok0101@gmail.com>
2023-07-03 21:09:37 +0900

    [codeSmell23] Jdk11까지 Value Object로 변경하는 리팩토링

---
+++ src/test/java/me/whiteship/refactoring/_06_mutable_data/_23_change_reference_to_value/TelephoneNumberTest.java
@@ -0,0 +1,18 @@
+package me.whiteship.refactoring._06_mutable_data._23_change_reference_to_value;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TelephoneNumberTest {
+
+    @Test
+    @DisplayName("두 객체가 동일한 객체인지 테스트를 수행한다.")
+    void isEqualObjectTest() throws Exception {
+        TelephoneNumber firstTelephoneNumber = new TelephoneNumber("area", "1");
+        TelephoneNumber secondTelephoneNumber = new TelephoneNumber("area", "1");
+
+        assertEquals(firstTelephoneNumber, secondTelephoneNumber);
+    }
+}

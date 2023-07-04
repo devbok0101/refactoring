20a4863fae7c7d9ff1860a381d924c5339549ea2
devbok0101 <devbok0101@gmail.com>
2023-07-04 23:21:14 +0900

    [codeSmell25] 추가로 AccountType 가독성 있게 리팩토링

--- src/main/java/me/whiteship/refactoring/_07_divergent_change/_25_move_function/AccountType.java
+++ src/main/java/me/whiteship/refactoring/_07_divergent_change/_25_move_function/AccountType.java
@@ -1,26 +1,32 @@
 package me.whiteship.refactoring._07_divergent_change._25_move_function;
 
 public class AccountType {
-    private boolean premium;
+    private final boolean premium;
 
     public AccountType(boolean premium) {
         this.premium = premium;
     }
 
-    public boolean isPremium() {
+    private boolean isPremium() {
         return this.premium;
     }
 
     double overdraftCharge(int daysOverdrawn) {
         if (this.isPremium()) {
-            final int baseCharge = 10;
-            if (daysOverdrawn <= 7) {
-                return baseCharge;
-            } else {
-                return baseCharge + (daysOverdrawn - 7) * 0.85;
-            }
-        } else {
-            return daysOverdrawn * 1.75;
+            return calculatePremiumOverDraftCharge(daysOverdrawn);
         }
+        return calculateNormalOverDraftCharge(daysOverdrawn);
+    }
+
+    private double calculateNormalOverDraftCharge(int daysOverdrawn) {
+        return daysOverdrawn * 1.75;
+    }
+
+    private double calculatePremiumOverDraftCharge(int daysOverdrawn) {
+        final int baseCharge = 10;
+        if (daysOverdrawn <= 7) {
+            return baseCharge;
+        }
+        return baseCharge + (daysOverdrawn - 7) * 0.85;
     }
 }

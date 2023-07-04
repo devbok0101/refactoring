4f7acb85273fb25d7683b18a9fbb15038ddf0f70
devbok0101 <devbok0101@gmail.com>
2023-07-04 23:36:29 +0900

    [codeSmell26] Person 객체의 telephoneNumber 메서드를 리팩토링

--- src/main/java/me/whiteship/refactoring/_07_divergent_change/_26_extract_class/Person.java
+++ src/main/java/me/whiteship/refactoring/_07_divergent_change/_26_extract_class/Person.java
@@ -11,18 +11,14 @@ public class Person {
     }
 
     public String telephoneNumber() {
-        return this.telephoneNumber.officeAreaCode() + " " + this.telephoneNumber.officeNumber();
+        return this.telephoneNumber.toString();
     }
 
     public String name() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public TelephoneNumber getTelephoneNumber() {
-        return telephoneNumber;
+        return this.telephoneNumber;
     }
 }
--- src/main/java/me/whiteship/refactoring/_07_divergent_change/_26_extract_class/TelephoneNumber.java
+++ src/main/java/me/whiteship/refactoring/_07_divergent_change/_26_extract_class/TelephoneNumber.java
@@ -1,27 +1,24 @@
 package me.whiteship.refactoring._07_divergent_change._26_extract_class;
 
 public class TelephoneNumber {
-    private String officeAreaCode;
-    private String officeNumber;
+    private String areaCode;
+    private String phoneNumber;
 
-    public TelephoneNumber(String officeAreaCode, String officeNumber) {
-        this.officeAreaCode = officeAreaCode;
-        this.officeNumber = officeNumber;
+    public TelephoneNumber(String areaCode, String phoneNumber) {
+        this.areaCode = areaCode;
+        this.phoneNumber = phoneNumber;
     }
 
-    public String officeAreaCode() {
-        return officeAreaCode;
+    public String getAreaCode() {
+        return areaCode;
     }
 
-    public void setOfficeAreaCode(String officeAreaCode) {
-        this.officeAreaCode = officeAreaCode;
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 
-    public String officeNumber() {
-        return officeNumber;
-    }
-
-    public void setOfficeNumber(String officeNumber) {
-        this.officeNumber = officeNumber;
+    @Override
+    public String toString() {
+        return this.areaCode+ " " + this.phoneNumber;
     }
 }

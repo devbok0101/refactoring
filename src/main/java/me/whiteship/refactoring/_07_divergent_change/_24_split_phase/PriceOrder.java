a55e1fc1ca9a18719a1ab293c2fddb2cd4c09e49
devbok0101 <devbok0101@gmail.com>
2023-07-04 22:59:42 +0900

    [codeSmell24] option + command + N 사용해서 inline variable 리팩토링

--- src/main/java/me/whiteship/refactoring/_07_divergent_change/_24_split_phase/PriceOrder.java
+++ src/main/java/me/whiteship/refactoring/_07_divergent_change/_24_split_phase/PriceOrder.java
@@ -3,12 +3,16 @@ package me.whiteship.refactoring._07_divergent_change._24_split_phase;
 public class PriceOrder {
 
     public double priceOrder(Product product, int quantity, ShippingMethod shippingMethod) {
+        final PriceData priceData = calculatePriceData(product, quantity);
+        return applyShippingCost(priceData, shippingMethod);
+    }
+
+    private PriceData calculatePriceData(Product product, int quantity) {
         final double basePrice = product.basePrice() * quantity;
         final double discount = Math.max(quantity - product.discountThreshold(), 0)
                 * product.basePrice() * product.discountRate();
 
-        final PriceData priceData = new PriceData(basePrice, discount, quantity);
-        return applyShippingCost(priceData, shippingMethod);
+        return new PriceData(basePrice, discount, quantity);
     }
 
     private double applyShippingCost(PriceData priceData, ShippingMethod shippingMethod) {

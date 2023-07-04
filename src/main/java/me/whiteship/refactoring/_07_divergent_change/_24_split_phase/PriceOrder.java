a2399481aed788e8f348a9a324899dcc8dfdca77
devbok0101 <devbok0101@gmail.com>
2023-07-04 22:48:34 +0900

    [codeSmell24] PriceData 라는 중간 객체 추가 사용

--- src/main/java/me/whiteship/refactoring/_07_divergent_change/_24_split_phase/PriceOrder.java
+++ src/main/java/me/whiteship/refactoring/_07_divergent_change/_24_split_phase/PriceOrder.java
@@ -6,10 +6,12 @@ public class PriceOrder {
         final double basePrice = product.basePrice() * quantity;
         final double discount = Math.max(quantity - product.discountThreshold(), 0)
                 * product.basePrice() * product.discountRate();
-        return applyShippingCost(quantity, shippingMethod, basePrice, discount);
+
+        final PriceData priceData = new PriceData();
+        return applyShippingCost(priceData, quantity, shippingMethod, basePrice, discount);
     }
 
-    private double applyShippingCost(int quantity, ShippingMethod shippingMethod, double basePrice, double discount) {
+    private double applyShippingCost(PriceData priceData, int quantity, ShippingMethod shippingMethod, double basePrice, double discount) {
         final double shippingPerCase = (basePrice > shippingMethod.discountThreshold()) ?
                 shippingMethod.discountedFee() : shippingMethod.feePerCase();
         final double shippingCost = quantity * shippingPerCase;

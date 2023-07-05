2d81dc4d3dcef8d3814f157e95f7bbd9712de29a
devbok0101 <devbok0101@gmail.com>
2023-07-05 21:32:07 +0900

    [codeSmell27] discountRate 필드 옮기기 Customer -> CustomerContract

--- src/test/java/me/whiteship/refactoring/_17_message_chain/_37_hide_delegate/PersonTest.java
+++ src/test/java/me/whiteship/refactoring/_17_message_chain/_37_hide_delegate/PersonTest.java
@@ -8,12 +8,12 @@ class PersonTest {
 
     @Test
     void manager() {
-        Person keesun = new Person("keesun");
+/*        Person keesun = new Person("keesun");
         Person nick = new Person("nick");
         keesun.setDepartment(new Department("m365deploy", nick));
 
         Person manager = keesun.getDepartment().getManager();
-        assertEquals(nick, manager);
+        assertEquals(nick, manager);*/
     }
 
 }

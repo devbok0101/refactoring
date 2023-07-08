f519a8d4e533d6743f3cf9836e3c4e72f8842e87
devbok0101 <devbok0101@gmail.com>
2023-07-08 20:39:57 +0900

    [codeSmell32] Employee 클래스에서 필요없어진 필드 제거 및 Switch 문 제거

--- src/main/java/me/whiteship/refactoring/_11_primitive_obsession/_32_replace_conditional_with_polymorphism/swtiches/Employee.java
+++ src/main/java/me/whiteship/refactoring/_11_primitive_obsession/_32_replace_conditional_with_polymorphism/swtiches/Employee.java
@@ -3,16 +3,7 @@ package me.whiteship.refactoring._11_primitive_obsession._32_replace_conditional
 import java.util.List;
 
 public class Employee {
-
-    protected String type;
-
     protected List<String> availableProjects;
-
-    public Employee(String type, List<String> availableProjects) {
-        this.type = type;
-        this.availableProjects = availableProjects;
-    }
-
     public Employee(List<String> availableProjects) {
         this.availableProjects = availableProjects;
     }
@@ -21,19 +12,10 @@ public class Employee {
     }
 
     public int vacationHours() {
-        return switch (type) {
-            case "full-time" -> 120;
-            case "part-time" -> 80;
-            case "temporal" -> 32;
-            default -> 0;
-        };
+        return 0;
     }
 
     public boolean canAccessTo(String project) {
-        return switch (type) {
-            case "full-time" -> true;
-            case "part-time", "temporal" -> this.availableProjects.contains(project);
-            default -> false;
-        };
+        return false;
     }
 }
